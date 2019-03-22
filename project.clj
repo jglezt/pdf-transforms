07ec84e097e584b6ea9853b002ccf4f4246ae2f8
jglezt <jglezt.ranger@gmail.com>
2019-03-22 16:16:47 -0600

    Updated to use newest dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject pdf-transforms "0.1.0"
+(defproject pdf-transforms "0.1.1"
   :description "A collections of functions for transforming pdf documents"
   :url "https://github.com/clearwater-analytics/pdf-transforms"
   :dependencies
-  [[org.clojure/clojure "1.9.0"]
-   [org.apache.pdfbox/pdfbox "2.0.3"]
-   [org.bouncycastle/bcmail-jdk15 "1.46"]
-   [org.bouncycastle/bcprov-jdk15 "1.46"]])
+  [[org.clojure/clojure "1.10.0"]
+   [org.apache.pdfbox/pdfbox "2.0.14"]
+   [org.bouncycastle/bcmail-jdk15on "1.61"]
+   [org.bouncycastle/bcprov-jdk15on "1.61"]])
